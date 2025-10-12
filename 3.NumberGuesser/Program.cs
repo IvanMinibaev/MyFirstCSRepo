a5cf60173c98630f8a65994300abfce760e3680c
IvanMinibaev <ivanminibaev60@gmail.com>
2025-10-13 00:16:27 +0300

    TASK3

---
+++ 3.NumberGuesser/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var rand = new Random();
+        int answer = rand.Next(10) + 1;
+        string input;
+        int guess;
+        bool end = false;
+
+        Console.WriteLine("Привет! я загадал число от 1 до 10. Спорим, что ты его не отгадаешь?");
+        while (!end)
+        {
+            input = Console.ReadLine();
+            while (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("Неверный ввод. Попробуй снова.");
+                input = Console.ReadLine();
+            }
+
+            if (guess < answer)
+                Console.WriteLine("Мое число больше");
+            if (guess > answer)
+                Console.WriteLine("Мое число меньше");
+            if (guess == answer)
+            {
+                Console.WriteLine("Угадал! Просто повезло >:(");
+                end = true;
+            }
+        }
+    }
+}
