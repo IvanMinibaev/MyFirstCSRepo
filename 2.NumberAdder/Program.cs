16351c26332fd484087183788440a621b4ca3a39
IvanMinibaev <ivanminibaev60@gmail.com>
2025-10-13 00:16:05 +0300

    TASK2

---
+++ 2.NumberAdder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+        int number = 1;
+        int sum = 0;
+        Console.WriteLine("Привет! Введи неколько чисел через enter, а я буду считать их сумму");
+        while (number != 0)
+        {
+            input = Console.ReadLine();
+            while (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Ошибка ввода, попробуй снова");
+                input = Console.ReadLine();
+            }
+            if (number != 0)
+            {
+                sum += number;
+                Console.WriteLine($"Пока что сумма равна {sum}");
+            }
+        }
+        Console.WriteLine($"Всего я насчитал {sum}. Пока - пока");
+    }
+}
