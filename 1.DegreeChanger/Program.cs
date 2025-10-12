8fb3192aed2dd76d899f52dfbfa1dafd9346fc6c
IvanMinibaev <ivanminibaev60@gmail.com>
2025-10-13 00:12:35 +0300

    TASK1

---
+++ 1.DegreeChanger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+     class Program
+    {
+        static void Main()
+        {
+            int celsius;
+            string input;
+
+            Console.WriteLine("Пожалуйста, введите температуру в градусах Цельсия");
+            input = Console.ReadLine();
+            while (! int.TryParse(input, out celsius))
+            {
+            Console.WriteLine("Ошибка ввода, попробуйте еще раз");
+            input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{celsius}C = {celsius * 1.8 + 32}F");
+            Console.ReadLine();
+        }
+    }
